953317cf4e82486658fe90b7c26fa21c0cd964da
unknown <fuzailafsar92@gmail.com>
2022-11-12 20:23:37 +0500

    issues fixed

--- assets/js/FireBase.js
+++ assets/js/FireBase.js
@@ -22,6 +22,14 @@ import {
   onSnapshot,
   orderBy,
 } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
+
 class FireBase {
   #auth;
   #db;
@@ -173,6 +181,39 @@ class FireBase {
     } catch (error) {
       throw error;
     }
+  }
+
+  #uploadFile(file, studentId) {
+    return new Promise((resolve, reject) => {
+      const uploadTask = uploadBytesResumable(
+        ref(getStorage(), `assets/images/students/${studentId}.png`),
+        file
+      );
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
   }
 }
 window.FireBase = FireBase;
